9eba9dd190b1d51374019daaba4830ed2101c3db
Reid McKenzie <rmckenzie92@gmail.com>
2013-07-25 13:38:21 -0500

    Take a stab at writing an assembler
    Sketch out an assembler which can index into the DCPU16 ISA description
    map and build single ops therefrom. Needs way more work as the FIXME
    blocks note.

--- src/toothpick/core.clj
+++ src/toothpick/core.clj
@@ -30,19 +30,83 @@
   ([m i s w]
      (let [sym (symbol s)]
        (eval `(def ~sym ~s))
-       (assoc-in m [:registers sym]
+       (assoc-in m [:registers s]
                  {:name sym
                   :code i
                   :width w})))
-  ([m i sym] 
+  ([m i sym]
      (reg m i sym 32)))
 
 (defn op
   ([m i s fmt & docs]
      (let [sym (symbol s)]
-       (eval `(defn ~sym [& args#] (apply vector ~s args#)))
-       (assoc-in m [:opcodes sym]
+       (eval `(defn ~sym [& args#]
+                (apply vector ~s args#)))
+       (assoc-in m [:opcodes s]
                  {:name sym
                   :code i
                   :fmt fmt
                   :docs (apply str docs)}))))
+
+(declare assemble)
+
+(defn assemble-op [isa state [op & params]]
+  ;; FIXME
+  ;;  This assemble-op is pretty targeted towards the DCPU16 and may not do
+  ;;  too well when faced with building code for ARM and x86. The main weirdness
+  ;;  with the DCPU16 is that instructions can be multi-word, and specific
+  ;;  parameters can actually be a + next word meaning that one op can streach
+  ;;  over three words :/
+  ;;
+  ;;  I deal with this by assuming that parameters are either standalone numbers
+  ;;  or are a list of (register . consts) where consts are to be postfixed
+  ;;  words. This behavior may have to change.
+
+  ;; FIXME
+  ;;  This assemble-op doesn't deal with labels as rvalues. rvalues probably
+  ;;  need to become op-like prefix expressions :/
+  (let [words (reduce (fn [wrds param]
+                        (if (list? param)
+                          (concat wrds (rest param))
+                          wrds))
+                      (list) params)
+        params (map (fn [x]
+                      (cond (list? x)
+                              (first x)
+                            (string? x)
+                              (get-in isa [:registers x :code])
+                            true
+                               x))
+                    params)]
+    (println params)
+    (concat (list (apply bit-fmt (get-in isa [:opcodes op :fmt])
+                                 (get-in isa [:opcodes op :code])
+                                 params))
+            words)))
+
+(defn assemble-form [[isa state words] form]
+  (if (vector? form)
+    (let [[op & args] form]
+      (case op
+        (LABEL label)
+          (let [[_label id] args]
+            [isa (assoc-in state [:labels id] (count words)) words])
+
+        ;; FIXME
+        ;;  needs to support an ALIGN directive
+
+        ;; FIXME
+        ;;  needs to support a PAD or SPACE directive
+
+        ;; FIXME
+        ;;  needs to support a WORD directive for literal values
+
+        ;; else case
+          [isa state (concat words (assemble-op isa state form))]))
+
+      (let [[_ state new-words] (apply assemble isa state form)]
+        [isa state (concat words new-words)])))
+
+(defn assemble [isa state & forms]
+  (reduce assemble-form
+          [isa state (list)] forms))
