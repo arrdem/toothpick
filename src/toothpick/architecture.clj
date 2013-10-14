02f74f13f1d7059f489794a17caf33b30d3a1ee0
arrdem <rmckenzie92@gmail.com>
2013-10-14 13:13:53 -0500

    Tweak: Ditch the big, useless comment

--- src/toothpick/architecture.clj
+++ src/toothpick/architecture.clj
@@ -11,46 +11,6 @@
 (defmacro define-architecture [name & forms]
   `(def ~name (-> {} ~@forms)))
 
-;; Idea: 
-;;
-;;    This is a new notation idea that totally struck me the night of
-;;    10/5. The notation used below uses predicates (or a constant)
-;;    and a number of bits to create an encoder/decoder pair for every
-;;    opcode in the ISA as specified. However the generated code is
-;;    not especially meaningful, and will almost certainly discard
-;;    notation specifics such as field names because the original
-;;    notation does not support it.
-;;
-;;    The solution to this problem, is to allow the user to detail the
-;;    symbolic name as well as the type of each opcode field. One
-;;    possible notation is
-;;
-;;    (opcode htl 32
-;;            (const-field :icode 6  0x00)
-;;            (const-field :dst   5  0x00)
-;;            (const-field :srca  5  0x00)
-;;            (const-field :srcb  5  0x00)
-;;            (const-field :lit   11 0x00))
-;;
-;;    But that's an easy instruction, it has a constant encode and
-;;    decode. How about another instruction with value ranges?
-;;
-;;    (opcode ld 32
-;;            (const-field     :icode 6 0x10)
-;;            (parameter-field :dst   5  register?)
-;;            (parameter-field :srca  5  register?)
-;;            (parameter-field :srcb  5  register?)
-;;            (parameter-field :const 11 integer?))
-;;
-;;    The great value proposition of this approach is that it allows
-;;    each instruction to have its own encode/decode pair which can
-;;    round-trip symbolic meaning. So if there's one special case
-;;    instruction which must have a bitfield set, or which supports
-;;    some other single bit flag that no other icode does this
-;;    notation naturally handles it because the decoder is generated
-;;    on a per instruction basis and is "fully aware" of the user's
-;;    desired notation.
-
 
 ;; subsystem for constructing icode descriptors
 ;;------------------------------------------------------------------------------
