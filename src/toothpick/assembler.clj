41e0c885b2a3a141ff5426a388d81f426680f2e0
Reid McKenzie <rmckenzie92@gmail.com>
2014-01-02 16:25:24 -0600

    Clean up the assembler for style and safety
     - Comment all the encoding functions
    
     - Fix a bug in list->bytecode where parameter order was dependent on
       the hash of the parameter name rather than the programmer specified
       parameter order.
    
     - Modify list->bytecode so that the operand list is taken as a
       separate seq in the tail position rather than being the & of the
       trailing arguments.
    
     - Add checks in map->bytecode to ensure that the incrementally
       destructured elements of the ISA and opcode maps rather than
       silently handing off garbage to encode-field.
    
     - Add an assertion in encode-field so that if the predicate function
       fails then an exception is thrown rather than silently return a nil
       value which will later fail to bit-or.

--- src/toothpick/assembler.clj
+++ src/toothpick/assembler.clj
@@ -1,10 +1,71 @@
-(ns toothpick.assembler)
+(ns toothpick.assembler
+  (:require [toothpick.core :refer [bit-mask-n]]))
 
-;--------------------------------------------------------------------------------
-; Define a prototype assembler
 
+;; Subsystem for using ISA descriptors to assemble instruction descriptor
+;; structures into bytes usable by a bytecode machine.
+;; ------------------------------------------------------------------------------
+(defn encode-field
+  "Encodes an instruction field, using the field descriptor map and a map of
+  parameter names to values. Returns an integer representing the encoded field."
+
+  [icode field val-map]
+  (case (:type field)
+    ;; case for encoding a constant field... Note that this does _not_
+    ;; make an effort to get a parameter from the parameters map,
+    ;; because its a constant!
+    (:const)
+      (bit-shift-left
+       (bit-and (bit-mask-n (:width field))
+                (:value field))
+       (:offset field))
+
+    ;; Because this case is a parameter that has to be encoded in, a
+    ;; parameter is fetched from the params map.
+    (:field)
+      (let [val (get val-map (:name field) 0)]
+        (assert ((:pred field) val)
+                (format "Failed to encode parameter %s" (name (:name field))))
+        (bit-shift-left
+         (bit-and (bit-mask-n (:width field))
+                  val)
+         (:offset field)))))
+
+
+(defn map->bytecode
+  "Compiles an instruction parameter map which the assembler would
+  produce. Returns the integer encoding of the specified opcode with the given
+  parameter values."
+
+  [isa opcode val-map]
+  (let [icode (get (:icodes isa) opcode)]
+    (assert icode 
+            (format "Could not get an icode for name %s" opcode))
+    (let [fields (:fields icode)]
+      (assert fields 
+              (format "Could not get icode fields for icode %s"
+                             (:name icode)))
+      (let [encoding (mapv #(encode-field icode %1 val-map) fields)]
+        (reduce bit-or 0 encoding)))))
+
+
+(defn list->bytecode
+  "Compiles a list that a programmer could actually type or generate
+  into an assembled word."
+
+  [isa [opcode & tail]]
+  (let [opcode (get-in isa [:icodes opcode])]
+    (assert opcode (format "Failed to find opcode in isa: %s" opcode))
+    (let [{:keys [fields params] :as icode} 
+          val-map (zipmap params tail)]
+      (map->bytecode isa opcode val-map))))
+
+
+;; Define a prototype assembler
+;; from here on down is all totally broken and shouldn't be used. yet.
+;;------------------------------------------------------------------------------
 (defn label? [form]
-  (or (and (list? form) 
+  (or (and (list? form)
            (= (first form) 'label)
            (keyword? (second form)))
       (keyword? form)))
@@ -28,7 +89,7 @@
          forms forms]
     (let [head (first forms)]
       (if (label? head)
-        (recur (assoc label-addr-map (label-symbol head) address) 
+        (recur (assoc label-addr-map (label-symbol head) address)
                address
                (rest forms))
         (if-not (empty? (rest forms))
@@ -46,7 +107,7 @@
 
 (defn assemble-form [isa label-map form]
   (let [[op & params] form
-        fun (get-in isa[:assemble (name op)])
+        fun (get-in isa [:assemble (name op)])
         params (map (partial resolve-param label-map) params)]
     (apply fun params)))
 
