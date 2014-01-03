a1ae4a76fa8c9089a6b5a45a7fc5c0f408489073
Reid McKenzie <rmckenzie92@gmail.com>
2014-01-03 15:26:13 -0600

    Bump to release status

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject toothpick "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject me.arrdem/toothpick "0.1.0"
+  :description "An assembler definition toolkit for Clojure"
+  :url "https://www.github.com/arrdem/toothpick"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
