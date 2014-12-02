72d4d6119175be5352564f0f94bc95110359ba1c
arrdem <me@arrdem.com>
2014-12-02 00:42:38 -0600

    Compact with macros, add ld

--- src/toothpick/isa/c16.clj
+++ src/toothpick/isa/c16.clj
@@ -14,62 +14,42 @@
   (let [v (dec (bit-shift-left 1 8))]
     (<= (- v) x v)))
 
-(def r_ZERO 7)
+(defmacro fmt-a [carry name op f]
+  `(opcode ~carry ~name
+           (const-field     :icode 4 ~op)
+           (const-field     :f     1 ~f)
+           (parameter-field :d     3 register?)
+           (parameter-field :a     3 register?)
+           (const-field     :_     2 0)
+           (parameter-field :b     3 register?)
+           ))
 
-(def c16
-  (-> {}
-      (opcode :add_rri
-              (const-field :icode 0 4)
-              (const-field :f     0 1)
-              (parameter-field :d 3 register?)
-              (parameter-field :a 3 register?)
-              (signed-parameter-field :i 5 imm5?))
+(defmacro fmt-b [carry name op f]
+  `(opcode ~carry ~name
+           (const-field     :icode 4 ~op)
+           (const-field     :f     1 ~f)
+           (parameter-field :d     3 register?)
+           (parameter-field :a     3 register?)
+           (parameter-field :i     5 imm5?)
+           ))
 
-      (opcode :add_rrr
-              (const-field :icode 0 4)
-              (const-field :f     1 1)
-              (parameter-field :d 3 register?)
-              (parameter-field :a 3 register?)
-              (const-field :_ 2 0)
-              (parameter-field :b 3 register?))
+(defmacro fmt-c [carry name op f]
+  `(opcode ~carry ~name
+           (const-field     :icode 4 ~op)
+           (const-field     :f     1 ~f)
+           (parameter-field :d     3 register?)
+           (parameter-field :i     8 imm8?)
+           ))
 
-      (opcode :sub_rri
-              (const-field :icode 1 4)
-              (const-field :f     0 1)
-              (parameter-field :d 3 register?)
-              (parameter-field :a 3 register?)
-              (signed-parameter-field :i 5 imm5?))
-
-      (opcode :sub_rrr
-              (const-field :icode 1 4)
-              (const-field :f     0 1)
-              (parameter-field :d 3 register?)
-              (parameter-field :a 3 register?)
-              (const-field :_     0 2)
-              (parameter-field :b 3 register?))
-
-      (opcode :brz_rri
-              (const-field :icode 0xF 4)
-              (const-field :f     0 1)
-              (parameter-field :d 3 register?)
-              (parameter-field :a 3 register?)
-              (signed-parameter-field :i 5 imm5?))
-
-      (opcode :brz_ri
-              (const-field :icode 0xF 4)
-              (const-field :f     1 1)
-              (parameter-field :d 3 register?)
-              (signed-parameter-field :i 8 imm8?))
-
-      (opcode :brnz_rri
-              (const-field :icode 0xE 4)
-              (const-field :f     0 1)
-              (parameter-field :d 3 register?)
-              (parameter-field :a 3 register?)
-              (signed-parameter-field :i 5 imm5?))
-
-      (opcode :brnz_ri
-              (const-field :icode 0xE 4)
-              (const-field :f     1 1)
-              (parameter-field :d 3 register?)
-              (signed-parameter-field :i 8 imm8?))))
+(define-architecture c16
+  (assoc :pcfn inc)  
+  (fmt-b :add_rri  0  0)
+  (fmt-a :add_rrr  0  1)
+  (fmt-b :sub_rri  1  0)
+  (fmt-a :sub_rrr  1  1)
+  (fmt-b :brnz_rri 14 0)
+  (fmt-c :brnz_ri  14 1)
+  (fmt-b :brz_rri  15 0)
+  (fmt-c :brz_ri   15 1)
+  (fmt-b :ld_rri   2r1010 0)
+  (fmt-c :ld_ri    2r1010 1))
