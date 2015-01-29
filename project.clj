58eb0cd4c9aed6d461c1408e44207f5430a08321
arrdem <me@arrdem.com>
2015-01-29 17:14:37 -0600

    Need core.match, detritus

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject me.arrdem/toothpick "0.1.1"
+(defproject me.arrdem/toothpick "0.2.0-SNAPSHOT"
   :description "An assembler definition toolkit for Clojure"
   :url "https://www.github.com/arrdem/toothpick"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [me.arrdem/detritus "0.2.1"]
+                 [org.clojure/core.match "0.3.0-alpha4"]])
