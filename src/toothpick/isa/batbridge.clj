1813787de681bd8799daf6b96393a9d61548f402
arrdem <me@arrdem.com>
2015-01-29 18:09:08 -0600

    Whitespace

--- src/toothpick/isa/batbridge.clj
+++ src/toothpick/isa/batbridge.clj
@@ -70,104 +70,104 @@
   ;; loads the word (+ a (* 4 x)) to register dst
   (opcode :ld 
           (const-field            :icode 6  0x10)
-          (unsigned-param-field        :d     5  register?)
-          (unsigned-param-field        :a     5  register?)
-          (unsigned-param-field        :x     5  register?)
-          (signed-param-field :i     11 literal?))
+          (unsigned-param-field   :d     5  register?)
+          (unsigned-param-field   :a     5  register?)
+          (unsigned-param-field   :x     5  register?)
+          (signed-param-field :i         11 literal?))
 
   ;; ST  0x11   010001 sssss aaaaa xxxxx iiiiiiiiiii
   ;; stores the word in register src to (+ a (* 4 x))
   (opcode :st
           (const-field            :icode 6  0x11)
-          (unsigned-param-field        :s     5  register?)
-          (unsigned-param-field        :a     5  register?)
-          (unsigned-param-field        :x     5  register?)
-          (signed-param-field :i     11 literal?))
+          (unsigned-param-field   :s     5  register?)
+          (unsigned-param-field   :a     5  register?)
+          (unsigned-param-field   :x     5  register?)
+          (signed-param-field :i         11 literal?))
   
   ;; IFLT 0x20  100000 _____ aaaaa bbbbb iiiiiiiiiii
   ;; execute the next instruction IFF (< a b)
   (opcode :iflt
           (const-field            :icode 6  0x20)
           (enforced-const-field   :_     5  0)
-          (unsigned-param-field        :a     5  register?)
-          (unsigned-param-field        :b     5  register?)
-          (signed-param-field :i     11 literal?))
+          (unsigned-param-field   :a     5  register?)
+          (unsigned-param-field   :b     5  register?)
+          (signed-param-field :i         11 literal?))
   
   ;; IFLE 0x21  100001 _____ aaaaa bbbbb iiiiiiiiiii
   ;; execute the next instruction IFF (<= a b)
   (opcode :ifle
           (const-field            :icode 6  0x21)
           (enforced-const-field   :_     5  0)
-          (unsigned-param-field        :a     5  register?)
-          (unsigned-param-field        :b     5  register?)
-          (signed-param-field :i     11 literal?))
+          (unsigned-param-field   :a     5  register?)
+          (unsigned-param-field   :b     5  register?)
+          (signed-param-field :i         11 literal?))
   
   ;; IFEQ 0x22  100010 _____ aaaaa bbbbb iiiiiiiiiii
   ;; execute the next instruction IFF (= a b)
   (opcode :ifeq
           (const-field            :icode 6  0x22)
           (enforced-const-field   :_     5  0)
-          (unsigned-param-field        :a     5  register?)
-          (unsigned-param-field        :b     5  register?)
-          (signed-param-field :i     11 literal?))
+          (unsigned-param-field   :a     5  register?)
+          (unsigned-param-field   :b     5  register?)
+          (signed-param-field :i         11 literal?))
   
   ;; IFNE 0x23  100013 _____ aaaaa bbbbb iiiiiiiiiii
   ;; execute the next instruction IFF (!= a b)
   (opcode :ifne
           (const-field            :icode 6  0x23)
           (enforced-const-field   :_     5  0)
-          (unsigned-param-field        :a     5  register?)
-          (unsigned-param-field        :b     5  register?)
-          (signed-param-field :i     11 literal?))
+          (unsigned-param-field   :a     5  register?)
+          (unsigned-param-field   :b     5  register?)
+          (signed-param-field :i         11 literal?))
       
-      ;; ADD  0x30  110000 ttttt aaaaa bbbbb iiiiiiiiiii
-      ;; stores (+ a b) to t
-      (bb-opcode :add 0x30)
+  ;; ADD  0x30  110000 ttttt aaaaa bbbbb iiiiiiiiiii
+  ;; stores (+ a b) to t
+  (bb-opcode :add 0x30)
       
-      ;; SUB  0x31  110001 ttttt aaaaa bbbbb iiiiiiiiiii
-      ;; stores (- a b) to t
-      (bb-opcode :sub 0x31)
+  ;; SUB  0x31  110001 ttttt aaaaa bbbbb iiiiiiiiiii
+  ;; stores (- a b) to t
+  (bb-opcode :sub 0x31)
       
-      ;; DIV  0x32  110010 ttttt aaaaa bbbbb iiiiiiiiiii
-      ;; stores the integer division of (/ a b) to t
-      (bb-opcode :div 0x32)
+  ;; DIV  0x32  110010 ttttt aaaaa bbbbb iiiiiiiiiii
+  ;; stores the integer division of (/ a b) to t
+  (bb-opcode :div 0x32)
       
-      ;; MOD  0x33  110011 ttttt aaaaa bbbbb iiiiiiiiiii
-      ;; stores the integer remainder (mod a b) to t
-      (bb-opcode :mod 0x33)
+  ;; MOD  0x33  110011 ttttt aaaaa bbbbb iiiiiiiiiii
+  ;; stores the integer remainder (mod a b) to t
+  (bb-opcode :mod 0x33)
       
-      ;; MUL  0x34  110100 ttttt aaaaa bbbbb iiiiiiiiiii
-      ;; stores the product (* a b) to t
-      (bb-opcode :mul 0x34)
+  ;; MUL  0x34  110100 ttttt aaaaa bbbbb iiiiiiiiiii
+  ;; stores the product (* a b) to t
+  (bb-opcode :mul 0x34)
       
-      ;; AND  0x35  110101 ttttt aaaaa bbbbb iiiiiiiiiii
-      ;; stores (bit-and a b) to t
-      (bb-opcode :and 0x35)
+  ;; AND  0x35  110101 ttttt aaaaa bbbbb iiiiiiiiiii
+  ;; stores (bit-and a b) to t
+  (bb-opcode :and 0x35)
       
-      ;; OR   0x36  110110 ttttt aaaaa bbbbb iiiiiiiiiii
-      ;; stores (bit-or a b) to t
-      (bb-opcode :or 0x36)
+  ;; OR   0x36  110110 ttttt aaaaa bbbbb iiiiiiiiiii
+  ;; stores (bit-or a b) to t
+  (bb-opcode :or 0x36)
 
-      ;; NAND 0x37  110111 ttttt aaaaa bbbbb iiiiiiiiiii
-      ;; stores (bit-not (bit-and a b)) to t
-      (bb-opcode :nand 0x37)
+  ;; NAND 0x37  110111 ttttt aaaaa bbbbb iiiiiiiiiii
+  ;; stores (bit-not (bit-and a b)) to t
+  (bb-opcode :nand 0x37)
       
-      ;; XOR  0x38  111000 ttttt aaaaa bbbbb iiiiiiiiiii
-      ;; stores (bit-xor a b) to t
-      (bb-opcode :xor 0x38)
+  ;; XOR  0x38  111000 ttttt aaaaa bbbbb iiiiiiiiiii
+  ;; stores (bit-xor a b) to t
+  (bb-opcode :xor 0x38)
       
-      ;; SL   0x3a  111010 ttttt aaaaa bbbbb iiiiiiiiiii
-      ;; stores (bit-shift-left a b) to t
-      (bb-opcode :sl 0x3a)
+  ;; SL   0x3a  111010 ttttt aaaaa bbbbb iiiiiiiiiii
+  ;; stores (bit-shift-left a b) to t
+  (bb-opcode :sl 0x3a)
       
-      ;; SR  0x3b  111011 ttttt aaaaa bbbbb iiiiiiiiiii
-      ;; stores the right shift of a, b bits to t
-      (bb-opcode :sr 0x3b)
+  ;; SR  0x3b  111011 ttttt aaaaa bbbbb iiiiiiiiiii
+  ;; stores the right shift of a, b bits to t
+  (bb-opcode :sr 0x3b)
       
-      ;; SAL  0x3c 111100 ttttt aaaaa bbbbb iiiiiiiiiii
-      ;; stores the arithmatic shift left of a b bits to t
-      (bb-opcode :sal 0x3c)
+  ;; SAL  0x3c 111100 ttttt aaaaa bbbbb iiiiiiiiiii
+  ;; stores the arithmatic shift left of a b bits to t
+  (bb-opcode :sal 0x3c)
 
-      ;; SAR  0x3d 111101 ttttt aaaaa bbbbb iiiiiiiiiii
-      ;; stores the arithmatic right shift of a by b bits to t
-      (bb-opcode :sar 0x3d))
+  ;; SAR  0x3d 111101 ttttt aaaaa bbbbb iiiiiiiiiii
+  ;; stores the arithmatic right shift of a by b bits to t
+  (bb-opcode :sar 0x3d))
